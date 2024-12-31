8bd846449e0acd121b3017ff338b7af844658f82
Lines That Connect <165847462+LinesThatConnect@users.noreply.github.com>
2024-12-30 21:36:31 -0500

    Added quick and dirty stuff to Combination

--- Combination.py
+++ Combination.py
@@ -1,4 +1,5 @@
 from manim import *
+from modules.interpolation import bounce
 
 
 def pow_out(pow):
@@ -39,15 +40,12 @@ class BinomialCoefficientDerivation(Scene):
             dot_3 = Dot(dots[state[2]].get_center(), 0.2, color=BLUE)
             label_3 = MathTex("k_3").move_to(dot_3.get_center() + 0.5 * DOWN, UP).set_color(BLUE)
             return VGroup(dot_1, label_1, dot_2, label_2, dot_3, label_3)
-        
-        points = get_points_for_state()
-
-        self.play(
-            Create(points)
-        )
 
 
+        iterations = 1
         def iterate_state():
+            nonlocal iterations
+            iterations += 1
             if state[2] < state[1] - 1:
                 state[2] += 1
             elif state[1] < state[0] - 1:
@@ -57,12 +55,104 @@ class BinomialCoefficientDerivation(Scene):
                 state[2] = 0
                 state[1] = 1
                 state[0] += 1
+
+
+        points = get_points_for_state()
+
+        self.play(
+            FadeIn(points[0], scale=3, rate_func = bounce()),
+            FadeIn(points[1], scale=0)
+        )
+
+        for i in range(3, 8):
+            while state[0] < i: iterate_state()
+            self.wait(0.75)
+            new_points = get_points_for_state()
+            points[0].become(new_points[0])
+            points[1].become(new_points[1])
+
+        self.wait(0.75)
+        self.play(
+            FadeIn(points[2], scale=3, rate_func = bounce()),
+            FadeIn(points[3], scale=0)
+        )
+
+        for i in range(2, 7):
+            while state[1] < i: iterate_state()
+            self.wait(0.75)
+            new_points = get_points_for_state()
+            points[2].become(new_points[2])
+            points[3].become(new_points[3])
+
+
+        self.wait(0.75)
+        while state[0] < 8: iterate_state()
+
+        new_points = get_points_for_state()
+        points[0].become(new_points[0])
+        points[1].become(new_points[1])
+        points[2].become(new_points[2])
+        points[3].become(new_points[3])
+
+        for i in range(2, 6):
+            while state[1] < i: iterate_state()
+            self.wait(0.75)
+            new_points = get_points_for_state()
+            points[2].become(new_points[2])
+            points[3].become(new_points[3])
         
-        for i in range(119):
+        self.wait(0.75)
+        new_points = get_points_for_state()
+        self.play(
+            FadeIn(points[4], scale=3, rate_func = bounce()),
+            FadeIn(points[5], scale=0)
+        )
+
+        self.remove(*points)
+        self.add(points)
+
+
+        for i in range(7):
             iterate_state()
+            self.wait(0.75)
             points.become(get_points_for_state())
+        
+
+
+        running_total = MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5)
+        running_total_title = Tex("Total").move_to(running_total.get_top() + UP * 0.2, DOWN)
+        self.play(
+            FadeIn(running_total, scale = 1.5),
+            FadeIn(running_total_title, scale = 1.5)
+        )
+
+
+
+        while state[0] < 10:
+            iterate_state()
+            if state[0] == 10: break
+            points.become(get_points_for_state())
+            running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
+
+            self.wait(0.15)
+            # self.play(
+            #     Transform(points, get_points_for_state(), run_time = 0.1, rate_func = pow_out(5))
+            # )
+
+
+
+        iterations = 1
+        state = [2, 1, 0]
+        points.become(get_points_for_state())
+        running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
+        
+        while state[0] < 10:
+            iterate_state()
+            if state[0] == 10: break
+            points.become(get_points_for_state())
+            running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
+
             self.wait(0.1)
             # self.play(
             #     Transform(points, get_points_for_state(), run_time = 0.1, rate_func = pow_out(5))
             # )
-            

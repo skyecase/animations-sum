992295102772b41fdbf8fe3c27eeb1d90a6d53d0
Lines That Connect <165847462+LinesThatConnect@users.noreply.github.com>
2025-02-02 20:14:57 -0500

    Finished Reciprocal

--- ToTheRight.py
+++ ToTheRight.py
@@ -1,7 +1,7 @@
 from manim import *
-from modules.custom_mobjects import FullscreenAxes, create_axes
-from modules.helpers import normalize_point_speed, fade_and_shift_in, create_updater_container
-from modules.interpolation import sin_smooth_in, bounce
+from modules.custom_mobjects import FullscreenAxes, create_arrow, create_axes, CustomArrow
+from modules.helpers import fade_and_shift_out, normalize_point_speed, fade_and_shift_in, create_updater_container
+from modules.interpolation import bounce, cubic_out
 
 
 class Reciprocal(MovingCameraScene):
@@ -43,12 +43,13 @@ class Reciprocal(MovingCameraScene):
 
         dot_0 = Dot(LEFT * 3, 0.12, color=BLUE)
         neg_dots = [Dot(dot_0.get_center() + RIGHT*DOT_SPREAD*i, 0.12, color=BLUE) for i in range(-2, 0)]
-        pos_dots = [Dot(dot_0.get_center() + RIGHT*DOT_SPREAD*i, 0.12, color=BLUE) for i in range(1, 5)]
+        pos_dots = [Dot(dot_0.get_center() + RIGHT*DOT_SPREAD*i, 0.12, color=BLUE) for i in range(1, 8)]
 
         s_n_text = MathTex("S(n)").move_to(dot_0.get_center() + DOWN, UP)
         arrow = Arrow(s_n_text.get_top(), dot_0.get_center(), buff=0.2)
+        really_big_text = Tex("($n$ is a really big whole number.)").scale(0.75).move_to(DOWN * 2.5)
 
-        for obj in [dot_0, s_n_text, arrow] + neg_dots + pos_dots: obj.set_z_index(2)
+        for obj in [dot_0, s_n_text, arrow, really_big_text] + neg_dots + pos_dots: obj.set_z_index(2)
 
 
         black_fade = Square(20, color=BLACK, fill_opacity=1)
@@ -61,6 +62,7 @@ class Reciprocal(MovingCameraScene):
                     *[FadeIn(dot, scale=3, rate_func=bounce()) for dot in neg_dots],
                     FadeIn(dot_0, scale=3, rate_func=bounce()),
                     fade_and_shift_in(VGroup(s_n_text, arrow), UP),
+                    fade_and_shift_in(really_big_text, UP),
                     lag_ratio = 0.2
                 ),
                 lag_ratio=0.5
@@ -71,4 +73,53 @@ class Reciprocal(MovingCameraScene):
 
 
 
+        arrows = [CustomArrow(dot_0.get_center() + i*DOT_SPREAD*RIGHT, dot_0.get_center() + (i+1)*DOT_SPREAD*RIGHT, angle=PI/2*(1-2*(i%2))) for i in range(0, 7)] # TODO: Remove i%2 part
+        self.add(*arrows)
+
+        np1_text = MathTex("+ f(n + 1)").scale(0.8).move_to((dot_0.get_center() + pos_dots[0].get_center())/2 + 0.6*UP, DOWN)
+
+        self.play(
+            arrows[0].end_vt.animate(rate_func=cubic_out).set_value(1),
+            FadeIn(np1_text, scale=0, shift=UP*0.5, rate_func=cubic_out),
+            FadeIn(pos_dots[0], scale=3, rate_func=bounce())
+        )
+
+        pretty_much_0_text = Tex("(Pretty much 0)").scale(0.5).move_to(np1_text.get_bottom())
+        self.play(
+            FadeIn(pretty_much_0_text, scale=0, shift = UP * pretty_much_0_text.height/2),
+            np1_text.animate.shift(UP * (pretty_much_0_text.height/2 + 0.1))
+        )
+
+
+        for i in range(1, len(arrows)):
+            f_text = MathTex("+ f(n + " + str(i+1) + ")").scale(0.8)
+            pretty_much_text = Tex("(Pretty much 0)").scale(0.5).move_to(f_text.get_bottom() + DOWN*0.1, UP)
+            group = VGroup(f_text, pretty_much_text)
+
+            dir = UP if i%2 == 0 else DOWN
+            group.move_to(dir * 0.2, -dir)
+            arrows[i].set_text(group)
         
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    arrows[0].start_vt.animate(rate_func=cubic_out).set_value(1),
+                    fade_and_shift_out(VGroup(np1_text, pretty_much_0_text), UP)
+                ),
+                LaggedStart(
+                    *[
+                        AnimationGroup(
+                            arrows[i].animation,
+                            FadeIn(pos_dots[i], scale = 3, rate_func=bounce())
+                        ) for i in range(1, len(arrows))
+                    ],
+                    lag_ratio = 0.25
+                ),
+                lag_ratio = 0.4
+            )
+        )
+
+
+        line = Line(8*LEFT, 8*RIGHT).set_color(YELLOW)
+        line.set_z_index(-1)
+        self.play(Create(line), rate_func=cubic_out)

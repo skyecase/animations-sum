e37b6dd92a969799b5d047328b59b99b893c0d82
Lines That Connect <165847462+LinesThatConnect@users.noreply.github.com>
2025-02-22 10:19:57 -0500

    'finished' BinomialCoefficientDerivation

--- Combination.py
+++ Combination.py
@@ -279,97 +279,73 @@ class BinomialCoefficientDerivation(Scene):
 
 
 
-class BinomialCoefficientSpecificChoice(Scene):
-    def construct(self):
-        text = MathTex("\\sum_{k_1 = 2}^{n - 1}", "\\sum_{k_2 = 1}^{k_1 - 1}", "\\sum_{k_3 = 0}^{k_2 - 1}", "1").move_to(UP * 2)
+        # ==================================
+        # CHOICES
+        # ==================================
 
-        text[0].set_color(RED)
-        text[1].set_color(GREEN)
-        text[2].set_color(BLUE)
+        u.remove_updater_index(0)
+        u.remove_updater_index(0)
 
-        self.add(text)
+        new_running_total = MathTex("1", ".").scale(1.5)
+        new_running_total.shift(one_digit_invisible[1].get_center() - new_running_total[1].get_center())
 
-
-        N = 10
-
-        dots = VGroup(*[Dot(RIGHT * i) for i in range(N)]).move_to(DOWN)
-        labels = [MathTex(i).move_to(dot.get_center() + 0.5 * UP, DOWN) for (i, dot) in enumerate(dots)]
-
-        self.add(dots, *labels)
-
-
-        state = [2, 1, 0]
-
-        def get_points_for_state():
-            dot_1 = Dot(dots[state[0]].get_center(), 0.2, color=RED)
-            label_1 = MathTex("k_1").move_to(dot_1.get_center() + 0.5 * DOWN, UP).set_color(RED)
-            dot_2 = Dot(dots[state[1]].get_center(), 0.2, color=GREEN)
-            label_2 = MathTex("k_2").move_to(dot_2.get_center() + 0.5 * DOWN, UP).set_color(GREEN)
-            dot_3 = Dot(dots[state[2]].get_center(), 0.2, color=BLUE)
-            label_3 = MathTex("k_3").move_to(dot_3.get_center() + 0.5 * DOWN, UP).set_color(BLUE)
-            return VGroup(dot_1, label_1, dot_2, label_2, dot_3, label_3)
-
-
+        iterations_vt.set_value(0.999)
         iterations = 1
-        def iterate_state():
-            nonlocal iterations
-            iterations += 1
-            if state[2] < state[1] - 1:
-                state[2] += 1
-            elif state[1] < state[0] - 1:
-                state[2] = 0
-                state[1] += 1
-            else:
-                state[2] = 0
-                state[1] = 1
-                state[0] += 1
-        
-
-        points = get_points_for_state()
-
-        running_total = MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5)
-        running_total_title = Tex("Total").move_to(running_total.get_top() + UP * 0.2, DOWN)
-        self.add(points, running_total, running_total_title)
-        
-
-        choices = [7, 5, 2]
-
-        choice_dots = VGroup(*[Circle(0.3, WHITE).move_to(dots[choice].get_center()) for choice in choices])
-
+        state = [2, 1, 0]
         self.play(
-            *[FadeIn(dot, scale=2) for dot in choice_dots]
+            running_total[0][0].animate.become(new_running_total[0][0]),
+            shrink_between(running_total[0][1:], new_running_total[0][0]),
+            points.animate(path_arc=-1).become(get_points_for_state())
         )
 
-        self.wait()
+        self.remove(*running_total[0])
+        running_total = new_running_total
+        self.add(running_total[0])
 
-        while state[0] < choices[0]:
-            iterate_state()
-            points.become(get_points_for_state())
-            running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
-            self.wait(0.1)
+        iterations_vt.set_value(0.999)
+        iterations = 1
+        state = [2, 1, 0]
 
-        self.wait()
 
-        while state[1] < choices[1]:
-            iterate_state()
-            points.become(get_points_for_state())
-            running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
-            self.wait(0.2)
-
-        self.wait()
-
-        while state[2] < choices[2]:
-            iterate_state()
-            points.become(get_points_for_state())
-            running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
-            self.wait(0.4)
+        choices = [7, 5, 2]
+        choice_dots = VGroup(*[Circle(0.3, YELLOW).move_to(dots[choice].get_center()) for choice in choices])
         
+        self.play(*[FadeIn(dot, scale=2) for dot in choice_dots])
+
+        u.add_updater(pool_and_point_updater)
+        u.add_updater(running_total_updater)
+
+
+        self.play(iterations_vt.animate.set_value(36), rate_func=linear, run_time = 5)
+        self.wait()
+
+        iterations_vt.increment_value(0.999)
+        self.play(iterations_vt.animate.set_value(46), rate_func=linear, run_time = 3)
+        self.wait()
+
+        iterations_vt.increment_value(0.999)
+        self.play(iterations_vt.animate.set_value(48), rate_func=linear, run_time = 0.75)
+        self.wait()
+
+        iterations_vt.increment_value(0.999)
+        self.play(iterations_vt.animate.set_value(120), rate_func=sin_smooth_in_out(0.8), run_time = 8)
         self.wait()
 
 
-        while state[0] < 10:
-            iterate_state()
-            if state[0] == 10: break
-            points.become(get_points_for_state())
-            running_total.become(MathTex(iterations).move_to(RIGHT * 5 + UP * 2).scale(1.5))
-            self.wait(0.1)
+        u.remove_updater_index(0)
+        u.remove_updater_index(0)
+
+
+        new_text = MathTex("\\sum_{k_1=0}^{x-1}", "\\sum_{k_2=0}^{k_1-1}", "\\sum_{k_3=0}^{k_2-1}", "1", "=").scale(1.2)
+        description = Tex("The number of possible ways\\\\to pick $3$ out of $x$ items").move_to((new_text[4].get_right() + 0.25)*RIGHT, LEFT)
+        VGroup(new_text, description).move_to(ORIGIN)
+
+        self.play(
+            VGroup(dots, points, *labels, choice_dots).animate(remover=True).shift(DOWN*2).set_color(BLACK),
+            FadeOut(VGroup(running_total, running_total_title), shift=UP),
+            LaggedStart(
+                Transform(text, new_text[:4]),
+                Write(VGroup(new_text[4], description)),
+                lag_ratio=0.7
+            )
+        )

3356693ee4cda105881fa7f56891c842a3ea3838
Lines That Connect <165847462+LinesThatConnect@users.noreply.github.com>
2025-03-23 22:33:13 -0400

    [wip] laying the groundwork for Graphs

--- FinalThoughts.py
+++ FinalThoughts.py
@@ -1,5 +1,7 @@
+import math
 from manim import *
 
+from modules.custom_mobjects import FullscreenAxes, create_axes
 from modules.helpers import fade_and_shift_in, fade_and_shift_out, grow_between, highlight_animation, morph_text
 
 
@@ -75,3 +77,74 @@ class FinalThoughts(Scene):
             fade_and_shift_out(VGroup(lim_text, text), UP),
             fade_and_shift_out(VGroup(example_1, example_2, example_3), DOWN)
         )
+
+
+
+def binomial_coefficient(n, k):
+    total = 1
+    for i in range(k):
+        total *= (n - i) / (i + 1)
+    return total
+
+
+
+def forward_difference(f, x, n):
+    """
+    f: function
+    x: input value
+    n: order
+    """
+    total = 0
+    for k in range(n + 1):
+        total += (1 if k%2 == 0 else -1) * binomial_coefficient(n, k) * f(x + n - k)
+    return total
+        
+
+
+def get_s(f, m, n):
+    def s(x):
+        floor_x = math.floor(x)
+
+        total = 0
+        if (x > 0):
+            for k in range(1, floor_x + 1):
+                total += f(k)
+        else:
+            for k in range(floor_x + 1, 1):
+                total -= f(k)
+        for k in range(1, n):
+            total += f(k + floor_x) - f(x + k)
+        for k in range(1, m + 1):
+            total += binomial_coefficient(x - floor_x, k) * forward_difference(f, n + floor_x, k-1)
+        return total
+    return s
+
+
+
+class Graphs(Scene):
+    def construct(self):
+
+
+        def f(x):
+            if (x >= 0): return math.sqrt(x) * math.sin(math.sqrt(x))
+            return -math.sqrt(-x) * math.sinh(math.sqrt(-x))
+        
+        axes = FullscreenAxes(self, LEFT*3 + DOWN*2, [0.7, 0.7])
+
+        def create_curve(function):
+            left = axes.point_to_coords(LEFT*7.12)[0]
+            right = axes.point_to_coords(RIGHT*7.12)[0]
+            return ParametricFunction(lambda t: axes.coords_to_point(t, function(t)), [left, right])
+
+
+        curve = create_curve(f)
+
+        self.play(
+            create_axes(self, axes)
+        )
+
+        self.play(Create(curve))
+
+        curve_2 = create_curve(get_s(f, 4, 100))
+
+        self.play(Create(curve_2))
